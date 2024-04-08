4f7baaaeb41e7ded3e8dba623ff421cdc17db902
mr-loop-1 <loopolecreator1@gmail.com>
2024-04-08 08:28:19 +0530

    add: more ui

--- frontend/app/pipeline/page.js
+++ frontend/app/pipeline/page.js
@@ -1,3 +1,16 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <main className="">
+            <Card className="w-[60%] mx-auto"></Card>
+        </main>
+    );
 }
